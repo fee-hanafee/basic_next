44663d662f1642aa7770094c8de95fc2c2e378dd
fee-hanafee <hanafee.pass@gmail.com>
2024-05-02 17:32:47 +0700

    set up regsiter

--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -1,21 +1,86 @@
+"use client";
 import React from "react";
+import Link from "next/link";
+import { useForm } from "react-hook-form";
+
 import Layout from "../ui/Layout";
 import Form from "../components/Form";
 import { TextField } from "@mui/material";
-import Link from "next/link";
 
 export default function RegisterPage() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    getValues,
+    setError,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = async (data) => {
+    if (!/[a-z]/gi.test(data.firstName))
+      setError("firstName", { type: "manual", message: true });
+    if (!/[a-z]/gi.test(data.lastName))
+      setError("lastName", { type: "manual", message: true });
+    if (!/^[a-z0-9_-]{3,15}$/gi.test(data.username))
+      setError("username", { type: "manual", message: true });
+
+    for (const i in data) {
+      if (!data[i]) {
+        setError(i, { type: "manual", message: true });
+      }
+    }
+
+    if (data?.password !== data?.confirmPassword) {
+      setError("confirmPassword", { type: "manual", message: true });
+      return;
+    }
+    if (errors) return;
+  };
+
   return (
     <Layout>
-      <Form title="SIGN UP" btnName="SIGN UP" variant="contained">
+      <Form
+        onSubmit={handleSubmit(onSubmit)}
+        title="SIGN UP"
+        btnName="SIGN UP"
+        variant="contained"
+      >
         <div className="flex gap-2">
-          <TextField placeholder="First Name" />
-          <TextField placeholder="Last Name" />
+          <TextField
+            label="First Name"
+            error={errors?.firstName?.message}
+            {...register("firstName")}
+          />
+          <TextField
+            error={errors?.lastName?.message}
+            label="Last Name"
+            {...register("lastName")}
+          />
         </div>
-        <TextField placeholder="Username" />
-        <TextField placeholder="Email" />
-        <TextField placeholder="Password" />
-        <TextField placeholder="Confirm Password" />
+        <TextField
+          error={errors?.username?.message}
+          label="Username"
+          {...register("username")}
+        />
+        <TextField
+          error={errors?.email?.message}
+          label="Email"
+          type="email"
+          {...register("email")}
+        />
+        <TextField
+          error={errors?.password?.message}
+          label="Password"
+          {...register("password")}
+          type="password"
+        />
+        <TextField
+          error={errors?.confirmPassword?.message}
+          label="Confirm Password"
+          {...register("confirmPassword")}
+          type="password"
+        />
       </Form>
       <div className="text-center">
         Already have an account ?{" "}
